a0daed172ff1b15ed2948f7f13ff141db5bb9572
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-09-12 19:04:08 +0530

    Add files via upload

---
+++ Alone/modules/helper_funcs/decorators.py
@@ -0,0 +1,204 @@
+"""
+MIT License
+
+Copyright (c) 2022 TEAMALONEOP 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from typing import List, Optional, Union
+
+from telegram.ext import (
+    CallbackQueryHandler,
+    CommandHandler,
+    InlineQueryHandler,
+    MessageHandler,
+)
+from telegram.ext.filters import MessageFilter
+
+from Alone import LOGGER
+from Alone import dispatcher as n
+from Alone.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+
+
+class AloneTelegramHandler:
+    def __init__(self, n):
+        self._dispatcher = n
+
+    def command(
+        self,
+        command: str,
+        filters: Optional[MessageFilter] = None,
+        admin_ok: bool = False,
+        pass_args: bool = False,
+        pass_chat_data: bool = False,
+        run_async: bool = True,
+        can_disable: bool = True,
+        group: Optional[Union[int, str]] = 40,
+    ):
+        def _command(func):
+            try:
+                if can_disable:
+                    self._dispatcher.add_handler(
+                        DisableAbleCommandHandler(
+                            command,
+                            func,
+                            filters=filters,
+                            run_async=run_async,
+                            pass_args=pass_args,
+                            admin_ok=admin_ok,
+                        ),
+                        group,
+                    )
+                else:
+                    self._dispatcher.add_handler(
+                        CommandHandler(
+                            command,
+                            func,
+                            filters=filters,
+                            run_async=run_async,
+                            pass_args=pass_args,
+                        ),
+                        group,
+                    )
+                LOGGER.debug(
+                    f"[AloneCMD] Loaded handler {command} for function {func.__name__} in group {group}"
+                )
+            except TypeError:
+                if can_disable:
+                    self._dispatcher.add_handler(
+                        DisableAbleCommandHandler(
+                            command,
+                            func,
+                            filters=filters,
+                            run_async=run_async,
+                            pass_args=pass_args,
+                            admin_ok=admin_ok,
+                            pass_chat_data=pass_chat_data,
+                        )
+                    )
+                else:
+                    self._dispatcher.add_handler(
+                        CommandHandler(
+                            command,
+                            func,
+                            filters=filters,
+                            run_async=run_async,
+                            pass_args=pass_args,
+                            pass_chat_data=pass_chat_data,
+                        )
+                    )
+                LOGGER.debug(
+                    f"[AloneCMD] Loaded handler {command} for function {func.__name__}"
+                )
+
+            return func
+
+        return _command
+
+    def message(
+        self,
+        pattern: Optional[str] = None,
+        can_disable: bool = True,
+        run_async: bool = True,
+        group: Optional[Union[int, str]] = 60,
+        friendly=None,
+    ):
+        def _message(func):
+            try:
+                if can_disable:
+                    self._dispatcher.add_handler(
+                        DisableAbleMessageHandler(
+                            pattern, func, friendly=friendly, run_async=run_async
+                        ),
+                        group,
+                    )
+                else:
+                    self._dispatcher.add_handler(
+                        MessageHandler(pattern, func, run_async=run_async), group
+                    )
+                LOGGER.debug(
+                    f"[AloneMSG] Loaded filter pattern {pattern} for function {func.__name__} in group {group}"
+                )
+            except TypeError:
+                if can_disable:
+                    self._dispatcher.add_handler(
+                        DisableAbleMessageHandler(
+                            pattern, func, friendly=friendly, run_async=run_async
+                        )
+                    )
+                else:
+                    self._dispatcher.add_handler(
+                        MessageHandler(pattern, func, run_async=run_async)
+                    )
+                LOGGER.debug(
+                    f"[AloneMSG] Loaded filter pattern {pattern} for function {func.__name__}"
+                )
+
+            return func
+
+        return _message
+
+    def callbackquery(self, pattern: str = None, run_async: bool = True):
+        def _callbackquery(func):
+            self._dispatcher.add_handler(
+                CallbackQueryHandler(
+                    pattern=pattern, callback=func, run_async=run_async
+                )
+            )
+            LOGGER.debug(
+                f"[AloneCALLBACK] Loaded callbackquery handler with pattern {pattern} for function {func.__name__}"
+            )
+            return func
+
+        return _callbackquery
+
+    def inlinequery(
+        self,
+        pattern: Optional[str] = None,
+        run_async: bool = True,
+        pass_user_data: bool = True,
+        pass_chat_data: bool = True,
+        chat_types: List[str] = None,
+    ):
+        def _inlinequery(func):
+            self._dispatcher.add_handler(
+                InlineQueryHandler(
+                    pattern=pattern,
+                    callback=func,
+                    run_async=run_async,
+                    pass_user_data=pass_user_data,
+                    pass_chat_data=pass_chat_data,
+                    chat_types=chat_types,
+                )
+            )
+            LOGGER.debug(
+                f"[AloneINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.__name__} | PASSES "
+                f"USER DATA: {pass_user_data} | PASSES CHAT DATA: {pass_chat_data} | CHAT TYPES: {chat_types}"
+            )
+            return func
+
+        return _inlinequery
+
+
+Alonecmd = AloneTelegramHandler(n).command
+Alonemsg = AloneTelegramHandler(n).message
+Alonecallback = AloneTelegramHandler(n).callbackquery
+Aloneinline = AloneTelegramHandler(n).inlinequery
